2c68f4f483c7d64e9f423d8f5693a2a5ac1fbccd
Lucas Alencar <57458709+OtakuGamerLucas@users.noreply.github.com>
2022-07-08 18:11:28 -0400

    Update settings.py

--- listadetarefas/settings.py
+++ listadetarefas/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=0$ove5r8jm!^^h4ty#yc%50mx62y*3rz=+g5nu@+bslh%s&(a'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
