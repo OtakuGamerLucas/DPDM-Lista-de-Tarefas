1de105731f5880c289e2820624bc314594e8f539
OtakuGamerLucas <lucasa.lucasd262@gmail.com>
2022-07-08 18:24:49 -0400

    1.1

--- listadetarefas/settings.py
+++ listadetarefas/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=0$ove5r8jm!^^h4ty#yc%50mx62y*3rz=+g5nu@+bslh%s&(a'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
